2c84fb690ccf6f9b6a473019ff49cb91a9748b06
paradise507 <parkhun507@gmail.com>
2025-05-15 12:53:26 +0900

    paradise

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:[본인의 포트번호]",
-      "http://localhost:[본인의 포트번호]",
+      "http://127.0.0.1:3000", // 실제 포트 번호로 수정
+      "http://localhost:3000", // 실제 포트 번호로 수정
     ],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
     credentials: true,
@@ -28,31 +28,48 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 
-const secretKey = "ozcodingschool";
+const secretKey = "ozcodingschool"; // 비밀 키
 
-// 클라이언트에서 post 요청을 받은 경우
+// 클라이언트에서 POST 요청을 받은 경우
 app.post("/", (req, res) => {
   const { userId, userPassword } = req.body;
   const userInfo = users.find(
     (el) => el.user_id === userId && el.user_password === userPassword
   );
-  // 유저정보가 없는 경우
+
+  // 유저 정보가 없는 경우
   if (!userInfo) {
-    res.status(401).send("로그인 실패");
+    return res.status(401).send("로그인 실패");
   } else {
-    // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
-    // 이곳에 코드를 작성하세요.
-    // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
-    // 이곳에 코드를 작성하세요.
+    // 1. 유저 정보가 있는 경우 accessToken을 발급하는 로직
+    const accessToken = jwt.sign({ user_id: userInfo.user_id }, secretKey, {
+      expiresIn: "1h", // 토큰 유효기간 설정
+    });
+
+    // 2. 응답으로 accessToken을 클라이언트로 전송
+    res.send({ accessToken });
   }
 });
 
-// 클라이언트에서 get 요청을 받은 경우
+// 클라이언트에서 GET 요청을 받은 경우
 app.get("/", (req, res) => {
-  // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
-  // 이곳에 코드를 작성하세요.
-  // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
-  // 이곳에 코드를 작성하세요.
+  // 3. req headers에 담겨 있는 accessToken을 검증하는 로직
+  const token = req.headers["authorization"]?.split(" ")[1]; // Bearer 토큰 형식에서 토큰 추출
+  if (!token) {
+    return res.status(401).send("토큰이 없습니다");
+  }
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      console.log(err); // 오류 로그 추가
+      return res.status(403).send("토큰 검증 실패");
+    }
+
+    // 4. 검증이 완료되면 유저 정보를 클라이언트로 전송
+    const userInfo = users.find((el) => el.user_id === user.user_id);
+    res.send(userInfo);
+  });
 });
 
 app.listen(3000, () => console.log("서버 실행!"));
+
